effbe0fa4f2b86c2ccf3b4ceeab557f0ca15fd47
muye1202 <muyejia2023@u.northwestern.edu>
2023-01-31 01:18:35 -0600

    The robot can play now

--- robo_simulator/robo_simulator/field.py
+++ robo_simulator/robo_simulator/field.py
@@ -1,17 +1,22 @@
 import math
 import rclpy
-import time as t
-import yaml
+import time as py_time
 from rclpy.node import Node
 from visualization_msgs.msg import MarkerArray, Marker
-from sensor_msgs.msg import JointState
-from geometry_msgs.msg import Point
-from turtlesim.msg import Pose
-from std_msgs.msg import Empty
-from geometry_msgs.msg import TransformStamped
+from tf_transformations import quaternion_from_euler
+from geometry_msgs.msg import Point, Pose2D, Quaternion, TransformStamped
+from std_srvs.srv import Empty
 from tf2_ros import TransformBroadcaster
-from ament_index_python.packages import get_package_share_path
 from enum import Enum, auto
+
+
+class State(Enum):
+    """Determine the state of the robot player."""
+
+    DRIBBLING = auto()
+    KICKING = auto()
+    DASHING = auto()
+    STOPPED = auto()
 
 
 class field(Node):
@@ -22,6 +27,16 @@ class field(Node):
         self.pub_marker = self.create_publisher(MarkerArray, "~/visualization_marker_array", 1)
         self.kick_sub = self.create_subscription(Point, "~/kick", self.kick_update, 10)
         self.pub_ball_marker = self.create_publisher(Marker, "~/visualization_marker", 1)
+        
+        # receive the velocity for the robot
+        self.vel_sub = self.create_subscription(Pose2D, '~/player_vel', self.vel_callback, 10)
+        
+        # send the position of the robot and ball
+        self.ball_pos_pub = self.create_publisher(Point, "~/ball_pos", 10)
+        self.robot_pos_pub = self.create_publisher(Pose2D, "~/robot_pos", 10)
+        
+        # reset robot and ball position
+        self.reset_srv = self.create_service(Empty, '~/reset', self.reset_callback)
         
         self.timer_period = 0.01
         self.timer = self.create_timer(self.timer_period, self.timer_callback)
@@ -36,26 +51,80 @@ class field(Node):
         ## as time progresses and then stops
         self.kick_power = 40.
         self.kick_dir = 0.
+        self.ball_startx = 0.1
+        self.ball_starty = 0.
         self.ball_posx = 0.1
         self.ball_posy = 0.
         self.last_vel = 0.
         
-        self.MAX_KICK_VEL = 10
+        self.MAX_KICK_VEL = 30
         self.MAX_STRENGTH = 100
-        self.BALL_DECAY = 0.02
+        self.BALL_DECAY = 0.4
 
         # marker for soccer ball
         self.ball = Marker()
         self.ball_marker()
         
+        # robo action param
+        self.startx = 0.
+        self.starty = 0.
+        self.posx = 0.
+        self.posy = 0.
+        self.ang = 0.
+        self.velx = 0.
+        self.vely = 0.
+        self.dash_speed = 0.
+        self.dash_dir = 0.
+        self.PLAYER_MAX_SPEED = 1
+        self.quaternion = Quaternion()
+        
+        self.player_state = State.STOPPED
+        self.ball_state = State.STOPPED
+        self.refresh_time = py_time.time()
+    
+    def reset_callback(self, _, resp):
+        """Reset robot and ball positions"""
+        self.posx = self.startx
+        self.posy = self.starty
+        self.ball_posx = self.ball_startx
+        self.ball_posy = self.ball_starty
+        
+        return resp
+    
+    def vel_callback(self, pose: Pose2D):
+        """
+        Receive robot dash command
+        
+        Input:
+            - pose: [speed, dir, 0]
+        """
+        self.dash_speed = (pose.x/100)*self.PLAYER_MAX_SPEED
+        self.dash_dir = (pose.y * math.pi)/180.
+        self.ang = (pose.y * math.pi)/180.
+        
+        self.velx = self.dash_speed * math.cos(self.dash_dir)
+        self.vely = self.dash_speed * math.sin(self.dash_dir)
+        
+        quat = quaternion_from_euler(0., 0., self.ang, 'ryxz')
+        self.quaternion.x = quat[0]
+        self.quaternion.y = quat[1]
+        self.quaternion.z = quat[2]
+        self.quaternion.w = quat[3]
+        self.refresh_time = py_time.time()
+        
+        # update the player as dashing
+        self.player_state = State.DASHING
+    
     def kick_update(self, point: Point):
         """kick: [pow, dir]"""
         self.kick_power = point.x
-        self.kick_dir = point.y
+        self.kick_dir = (point.y * math.pi)/180.
         
         # when receiving new effective kick
         # the ball will move the farthest this time
         self.last_vel = (self.kick_power/self.MAX_STRENGTH)*self.MAX_KICK_VEL
+        
+        self.ball_state = State.KICKING
 
     def calc_ball_pos(self, last_vel):
         """Calculate ball position at each time step"""
@@ -71,117 +140,125 @@ class field(Node):
         # calc new moving dist from last moving dist.
         new_vel = last_vel*self.BALL_DECAY
         self.last_vel = new_vel
+    
+    def player_to_ball_dist(self):
         
+        return math.sqrt((self.ball_posx - self.posx)**2 + 
+                         (self.ball_posy - self.posy)**2)
+    
     def marker_generate(self):
         """Create markers array for arena."""
         # 1st long side of the arena
-        arena_size = 11
-        x_pos = -0.5*arena_size + 1.5
-        y_pos = -0.5*arena_size + 1.5
+        arena_size = 5.5
         z_pos = 0.
-        for i in range(arena_size):
-            marker_shape = Marker()
-            marker_shape.header.frame_id = "nusim/world"
-            marker_shape.ns = "long_side_0" + str(i)
-            marker_shape.id = 0
-            marker_shape.action = 0
-            marker_shape.type = 1
-            marker_shape.color.r = 0.
-            marker_shape.color.g = 1.
-            marker_shape.color.b = 1.
-            marker_shape.color.a = 1.0
-            marker_shape.scale.x = 0.5
-            marker_shape.scale.y = 0.5
-            marker_shape.scale.z = 0.5
-            marker_shape.frame_locked = False
 
-            marker_shape.pose.position.x = x_pos
-            marker_shape.pose.position.y = y_pos
-            marker_shape.pose.position.z = z_pos
-            x_pos += 1
+        marker_shape = Marker()
+        marker_shape.header.frame_id = "nusim/world"
+        marker_shape.ns = "long_side_0"
+        marker_shape.id = 0
+        marker_shape.action = 0
+        marker_shape.type = 1
+        marker_shape.color.r = 0.
+        marker_shape.color.g = 1.
+        marker_shape.color.b = 1.
+        marker_shape.color.a = 1.0
+        marker_shape.scale.x = 2*arena_size
+        marker_shape.scale.y = 0.2
+        marker_shape.scale.z = 0.5
+        marker_shape.frame_locked = False
 
-            self.marker_arr.markers.append(marker_shape)
+        marker_shape.pose.position.x = 0.
+        marker_shape.pose.position.y = -0.5*arena_size
+        marker_shape.pose.position.z = z_pos
 
-        x_pos = -0.5*arena_size + 1.5
-        y_pos = 0.5*arena_size + 1.5
-        z_pos = 0.
-        # 2nd long side of the arena
-        for i in range(arena_size):
-            marker_shape = Marker()
-            marker_shape.header.frame_id = "nusim/world"
-            marker_shape.ns = "long_side_1" + str(i)
-            marker_shape.id = 1
-            marker_shape.action = 0
-            marker_shape.type = 1
-            marker_shape.color.r = 0.
-            marker_shape.color.g = 1.
-            marker_shape.color.b = 1.
-            marker_shape.color.a = 1.0
-            marker_shape.scale.x = 0.5
-            marker_shape.scale.y = 0.5
-            marker_shape.scale.z = 0.5
-            marker_shape.frame_locked = False
+        self.marker_arr.markers.append(marker_shape)
 
-            marker_shape.pose.position.x = x_pos
-            marker_shape.pose.position.y = y_pos
-            marker_shape.pose.position.z = z_pos
-            x_pos += 1
+        marker_shape = Marker()
+        marker_shape.header.frame_id = "nusim/world"
+        marker_shape.ns = "long_side_1"
+        marker_shape.id = 1
+        marker_shape.action = 0
+        marker_shape.type = 1
+        marker_shape.color.r = 0.
+        marker_shape.color.g = 1.
+        marker_shape.color.b = 1.
+        marker_shape.color.a = 1.0
+        marker_shape.scale.x = 2*arena_size
+        marker_shape.scale.y = 0.2
+        marker_shape.scale.z = 0.5
+        marker_shape.frame_locked = False
 
-            self.marker_arr.markers.append(marker_shape)
+        marker_shape.pose.position.x = 0.
+        marker_shape.pose.position.y = 0.5*arena_size
+        marker_shape.pose.position.z = z_pos
+
+        self.marker_arr.markers.append(marker_shape)
 
         # 1st y side of the arena
-        x_pos = -0.5*arena_size + 1.5
-        y_pos = -0.5*arena_size + 1.5
-        z_pos = 0.
-        for i in range(arena_size):
-            marker_shape = Marker()
-            marker_shape.header.frame_id = "nusim/world"
-            marker_shape.ns = "y_side_0" + str(i)
-            marker_shape.id = 2
-            marker_shape.action = 0
-            marker_shape.type = 1
-            marker_shape.color.r = 0.
-            marker_shape.color.g = 1.
-            marker_shape.color.b = 1.
-            marker_shape.color.a = 1.0
-            marker_shape.scale.x = 0.5
-            marker_shape.scale.y = 0.5
-            marker_shape.scale.z = 0.5
-            marker_shape.frame_locked = False
+        marker_shape = Marker()
+        marker_shape.header.frame_id = "nusim/world"
+        marker_shape.ns = "y_side_0"
+        marker_shape.id = 2
+        marker_shape.action = 0
+        marker_shape.type = 1
+        marker_shape.color.r = 0.
+        marker_shape.color.g = 1.
+        marker_shape.color.b = 1.
+        marker_shape.color.a = 1.0
+        marker_shape.scale.x = 0.2
+        marker_shape.scale.y = arena_size
+        marker_shape.scale.z = 0.5
+        marker_shape.frame_locked = False
 
-            marker_shape.pose.position.x = x_pos
-            marker_shape.pose.position.y = y_pos
-            marker_shape.pose.position.z = z_pos
-            y_pos += 1
+        marker_shape.pose.position.x = -arena_size
+        marker_shape.pose.position.y = 0.
+        marker_shape.pose.position.z = z_pos
 
-            self.marker_arr.markers.append(marker_shape)
+        self.marker_arr.markers.append(marker_shape)
 
-        x_pos = 0.5*arena_size + 1.5
-        y_pos = -0.5*arena_size + 1.5
-        z_pos = 0.
-        # 2nd y side of arena
-        for i in range(arena_size+1):
-            marker_shape = Marker()
-            marker_shape.header.frame_id = "nusim/world"
-            marker_shape.ns = "y_side_1" + str(i)
-            marker_shape.id = 3
-            marker_shape.action = 0
-            marker_shape.type = 1
-            marker_shape.color.r = 0.
-            marker_shape.color.g = 1.
-            marker_shape.color.b = 1.
-            marker_shape.color.a = 1.0
-            marker_shape.scale.x = 0.5
-            marker_shape.scale.y = 0.5
-            marker_shape.scale.z = 0.5
-            marker_shape.frame_locked = False
+        # goal side marker up
+        marker_shape = Marker()
+        marker_shape.header.frame_id = "nusim/world"
+        marker_shape.ns = "goal_up"
+        marker_shape.id = 3
+        marker_shape.action = 0
+        marker_shape.type = 1
+        marker_shape.color.r = 0.
+        marker_shape.color.g = 1.
+        marker_shape.color.b = 1.
+        marker_shape.color.a = 1.0
+        marker_shape.scale.x = 0.2
+        marker_shape.scale.y = 0.4*arena_size
+        marker_shape.scale.z = 0.5
+        marker_shape.frame_locked = False
 
-            marker_shape.pose.position.x = x_pos
-            marker_shape.pose.position.y = y_pos
-            marker_shape.pose.position.z = z_pos
-            y_pos += 1
+        marker_shape.pose.position.x = arena_size
+        marker_shape.pose.position.y = 0.3*arena_size
+        marker_shape.pose.position.z = z_pos
 
-            self.marker_arr.markers.append(marker_shape)
+        self.marker_arr.markers.append(marker_shape)
+        
+        # goal side marker down
+        marker_shape = Marker()
+        marker_shape.header.frame_id = "nusim/world"
+        marker_shape.ns = "goal_down"
+        marker_shape.id = 3
+        marker_shape.action = 0
+        marker_shape.type = 1
+        marker_shape.color.r = 0.
+        marker_shape.color.g = 1.
+        marker_shape.color.b = 1.
+        marker_shape.color.a = 1.0
+        marker_shape.scale.x = 0.2
+        marker_shape.scale.y = 0.4*arena_size
+        marker_shape.scale.z = 0.5
+        marker_shape.frame_locked = False
+
+        marker_shape.pose.position.x = arena_size
+        marker_shape.pose.position.y = -0.3*arena_size
+        marker_shape.pose.position.z = z_pos
+
+        self.marker_arr.markers.append(marker_shape)
     
     def ball_marker(self):
         self.ball.header.frame_id = "nusim/world"
@@ -201,15 +278,62 @@ class field(Node):
         self.ball.pose.position.y = self.ball_posy
     
     def timer_callback(self):
+        # calculate time elapsed from last receiving dash cmd
+        curr_time = py_time.time()
+        robot_travel_time = curr_time - self.refresh_time
         # publish arena marker
         self.pub_marker.publish(self.marker_arr)
-        
+
+        # calculate current robot position
+        self.posx += robot_travel_time * self.velx
+        self.posy += robot_travel_time * self.vely
+
         # update ball position at each cycle
         # get kick pow and dir from subsriber
         self.calc_ball_pos(self.last_vel)
-        self.get_logger().info("ball velocity: " + str(self.last_vel))
+
+        # if the player does not kick, the ball
+        # should move with the player
+        ball_to_robo = self.player_to_ball_dist()
+        if (self.player_state == State.DASHING and 
+           not self.ball_state == State.KICKING and
+           ball_to_robo <= 0.1):
+            # set ball position
+            self.ball_posx = self.posx + ball_to_robo*math.cos(self.ang)
+            self.ball_posy = self.posy + ball_to_robo*math.sin(self.ang)
+
+        # publish the ball marker
         self.ball_marker()
         self.pub_ball_marker.publish(self.ball)
+        
+        # publish transform for the robot
+        time = self.get_clock().now().to_msg()
+        world_robot = TransformStamped()
+        world_robot.header.stamp = time
+        world_robot.header.frame_id = "nusim/world"
+        world_robot.child_frame_id = "purple/base_footprint"
+        world_robot.transform.translation.x = self.posx
+        world_robot.transform.translation.y = self.posy
+        world_robot.transform.rotation.x = self.quaternion.x
+        world_robot.transform.rotation.y = self.quaternion.y
+        world_robot.transform.rotation.z = self.quaternion.z
+        world_robot.transform.rotation.w = self.quaternion.w
+        self.broadcaster.sendTransform(world_robot)
+        
+        # publish the position of the robot and ball
+        r_pos = Pose2D()
+        r_pos.x = self.posx
+        r_pos.y = self.posy
+        r_pos.theta = math.degrees(self.ang)
+        self.robot_pos_pub.publish(r_pos)
+        
+        ball_pos = Point()
+        ball_pos.x = self.ball_posx
+        ball_pos.y = self.ball_posy
+        self.ball_pos_pub.publish(ball_pos)
+        
+        self.player_state = State.STOPPED
+        self.ball_state = State.STOPPED
 
 
 def main(args=None):
--- robo_simulator/robo_simulator/simulator.py
+++ robo_simulator/robo_simulator/simulator.py
@@ -1,16 +1,20 @@
-from std_msgs.msg import Empty
 import rclpy
 import math
-import yaml
+import numpy as np
 from rclpy.node import Node
-from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
-from tf2_ros import TransformBroadcaster
 from geometry_msgs.msg import TransformStamped
 from sensor_msgs.msg import JointState
-from geometry_msgs.msg import Pose2D, Vector3, Point, TwistWithCovariance
-from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Pose2D, Quaternion, Point
 from ament_index_python.packages import get_package_share_path
 from enum import Enum, auto
+
+
+class State(Enum):
+    """Determine the state of the robot player."""
+
+    BALL_KICKABLE = auto()
+    IDLE = auto()
+
 
 class rs_simulator(Node):
     """Publish a robot position and ball marker position"""
@@ -18,48 +22,110 @@ class rs_simulator(Node):
     def __init__(self):
         super().__init__("rs_simulator")
         
-        timer_period = 0.05
+        timer_period = 0.01
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        
-        # receive the velocity for the robot
-        self.vel_sub = self.create_subscription(Pose2D, '~/player_vel', self.vel_callback, 10)
         
         # send kick cmd
         self.kick_pub = self.create_publisher(Point, "field/kick", 10)
         
-        # publish the location of the robot
-        self.broadcaster = TransformBroadcaster(self)
+        # send robot velocity cmd
+        self.vel_pub = self.create_publisher(Pose2D, "field/player_vel", 10)
         
-        # robo action param
-        self.startx = 0.
-        self.starty = 0.
-        self.posx = 0.
-        self.posy = 0.
-        self.ang = 0.
-        self.kick = Point()
+        # receive ball and robot position
+        self.ball_sub = self.create_subscription(Point, "field/ball_pos", self.ball_callback, 10)
+        self.robot_sub = self.create_subscription(Pose2D, "field/robot_pos", self.robot_callback, 10)
         
-    def vel_callback(self, pose: Pose2D):
-        self.posx = pose.x
-        self.posy = pose.y
-        self.ang = pose.theta   
+        # field params
+        self.ball_pos = Point()
+        self.robot_pos = Pose2D()
+        self.robo_state = State.IDLE
+    
+    def ball_callback(self, ball_pos: Point):
+        self.ball_pos = ball_pos
+        
+    def robot_callback(self, robo_pos: Pose2D):
+        self.robot_pos = robo_pos
+        self.get_logger().info("robot direction: " + str(robo_pos.theta))
+        
+    def player_to_ball_dist(self):
+        
+        return math.sqrt((self.ball_pos.x - self.robot_pos.x)**2 + 
+                         (self.ball_pos.y - self.robot_pos.y)**2)
+    
+    def dash(self, power, dir):
+        dash_cmd = Pose2D()
+        dash_cmd.x = float(power)
+        dash_cmd.y = float(dir)
+        self.vel_pub.publish(dash_cmd)
+        
+    def kick(self, power, dir):
+        kick_cmd = Point()
+        kick_cmd.x = float(power)
+        kick_cmd.y = float(dir)
+        self.kick_pub.publish(kick_cmd)
+        
+    def turning_angle(self, ball: Point, robot: Pose2D):
+        # the angle is 0 to PI and -PI to 0
+        dely = ball.y - robot.y
+        delx = ball.x - robot.x
+        ball_to_robot = math.degrees(math.atan2(dely, delx))
+        robot_facing = self.robot_pos.theta
+        
+        self.get_logger().info("robot direction: " + str(robot_facing) + " " + str(ball_to_robot))
+        
+        if robot_facing <= 0 and 0 <= ball_to_robot <= 90:
+            if -(180-ball_to_robot) < robot_facing:
+                return abs(robot_facing) + ball_to_robot
+            else:
+                return -(360 - abs(robot_facing) - ball_to_robot)
+            
+        elif robot_facing <= 0 and 90 < ball_to_robot <= 180:
+            if ball_to_robot - robot_facing < 180:
+                return ball_to_robot - robot_facing
+            else:
+                return -(360-ball_to_robot - robot_facing)
+            
+        elif 180 >= robot_facing >= 0 and 0 <= ball_to_robot <= 180:
+            if robot_facing > ball_to_robot:
+                return -(robot_facing-ball_to_robot)
+            else:
+                return ball_to_robot - robot_facing
+        
+        elif 180 >= robot_facing >= 0 and ball_to_robot <= 0:
+            if abs(ball_to_robot) + robot_facing < 180:
+                return -abs(ball_to_robot) - robot_facing
+            else:
+                return 360 - (abs(ball_to_robot) + robot_facing)
+            
+        return 0
+        
+    def follow_ball(self, ball_pos: Point):
+        # dash towards the ball
+        dash_dir = self.turning_angle(ball_pos, self.robot_pos)
+        dash_pow = 70
+        
+        #self.get_logger().info("dash direction: " + str(dash_dir))
+        self.dash(dash_pow, dash_dir)
     
     def timer_callback(self):
-        self.startx += 0.01
-
-        # publish transform for the robot
-        time = self.get_clock().now().to_msg()
-        world_robot = TransformStamped()
-        world_robot.header.stamp = time
-        world_robot.header.frame_id = "nusim/world"
-        world_robot.child_frame_id = "purple/base_footprint"
-        world_robot.transform.translation.x = self.startx
-        world_robot.transform.translation.y = self.starty
-        self.broadcaster.sendTransform(world_robot)
+        # # TEST PASSED: robot dashing forward
+        # #       but not kicking the ball
+        # #       pose2D: [power, dir, 0.]
+        # dash_cmd = Pose2D()
+        # dash_cmd.x = 10.
+        # dash_cmd.y = 30.
+        # self.vel_pub.publish(dash_cmd)
         
-        # TEST: send kick cmd -> [pow, dir]
-        self.kick.x = 20.
-        self.kick.y = 0.
-        self.kick_pub.publish(self.kick)
+        # TEST: kick the ball and run to it
+        if self.player_to_ball_dist() <= 0.1:
+            self.robo_state = State.BALL_KICKABLE
+
+        if self.robo_state == State.BALL_KICKABLE:
+            kick_dir = np.random.uniform(low=-55, high=55)
+            self.kick(70., kick_dir)
+            self.robo_state = State.IDLE
+        
+        self.follow_ball(self.ball_pos)
 
 def main(args=None):
     rclpy.init(args=args)
